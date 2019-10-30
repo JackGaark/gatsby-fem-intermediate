4aac86b78e252afbe10417346f1209c724f94195
jack gaarkeuken <jack.gaarkeuken@gmail.com>
2019-10-30 17:40:20 -0400

    Some work

--- packages/gatsby-theme-docs/gatsby-node.js
+++ packages/gatsby-theme-docs/gatsby-node.js
@@ -25,6 +25,85 @@ exports.createSchemaCustomization = ({ actions }) => {
     updated: Date! @dateformat
     body: String!
 
+    }
+  `);
+};
+
+exports.onCreateNode = ({node, actions, getNode, createNodeId }, options) => {
+  const { basePath } = withDefaults(options);
+  const parent = getNode(node.parent);
+
+  //only work on MDX files that were loaded by this theme
+  if(
+    node.internal.type !== 'Mdx' ||
+    parent.sourceInstanceName !== 'gatsby-theme-docs'
+  ) {
+    return;
   }
-  `)
-}
+
+  //Treat `index.mdx` link `index.html` (i.e. `docs/` vs. `docs/index/`);
+  const pageName = parent.name !== 'index' ? parent.name : '';
+
+  actions.createNode({
+    id: createNodeId(`DocsPage-${node.id}`),
+    title: node.frontmatter.title || parent.name,
+    updated: parent.modifiedTime,
+    path: path.join('/', basePath, parent.relativeDirectory, pageName),
+    parent: node.id,
+    internal: {
+      type: 'DocsPage',
+      contentDigest: node.internal.contentDigest,
+    }
+  });
+};
+
+exports.createResolvers = ({ createResolvers}) => {
+  createResolvers({
+    DocsPage: {
+      body: {
+        type: 'String!',
+        resolve: (source, args, context, info) => {
+        //Load the resolver for the `Mdx` type `body` field.
+        const type = info.schema.getType('Mdx');
+        const mdxFields = type.getFields();
+        const resolver = mdxFields.body.resolve;
+
+        const mdxNode = context.nodeModel.getNodeById({ id: source.parent });
+
+        return resolver(mdxNode, args, context, {
+          fieldName: 'body',
+          });
+        },
+      },
+    },
+  });
+};
+
+exports.createPages = async ({actions, graphql, reporter }) => {
+  const result = await graphql(`
+    query {
+      allDocsPage {
+        nodes {
+          id
+          path
+        }
+      }
+    }
+  `);
+
+  if (result.errors) {
+    resporter.panic('error loading docs', result.errors);
+  }
+
+  const pages = result.data.allDocsPage.nodes;
+
+  pages.forEach(page => {
+    actions.createPage({
+      path: page.path,
+      component: require.resolve('./src/templates/docs-page-template.js'),
+      context: {
+        pageID: page.id,
+      },
+    });
+  });
+};
---
+++ packages/gatsby-theme-docs/src/components/docs-page.js
@@ -0,0 +1,22 @@
+/** @jsx jsx */
+import {jsx} from 'theme-ui';
+import {MDXRenderer} from 'gatsby-plugin-mdx';
+import Layout from './layout';
+
+const DocsPage = ({page }) => (
+  <Layout>
+    <h1>{page.title}</h1>
+    <MDXRenderer>{page.body}</MDXRenderer>
+    <p
+      sx={{
+        borderTop: theme => `1px solid ${theme.colors.muted}`,
+        color: 'muted',
+        fontSize: 14,
+        mt: 2,
+        pt: 2,
+      }}
+    >This page was updated{page.updated}.</p>
+  </Layout>
+)
+
+export default DocsPage;
